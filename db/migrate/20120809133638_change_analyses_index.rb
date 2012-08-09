eaf4ac8041e27320d0607b98e61d1ab1d22e534c
Laurens Fridael <lfridael@xs4all.nl>
2012-08-09 19:07:11 +0200

    Another fix for analyses unique index migration.

--- db/migrate/20120809133638_change_analyses_index.rb
+++ db/migrate/20120809133638_change_analyses_index.rb
@@ -2,13 +2,15 @@ class ChangeAnalysesIndex < ActiveRecord::Migration
   def up
     # See: http://www.eimermusic.com/code/rails-migration-of-indexes/
     change_table :analyses do |t|
-      t.remove_index :name => 'index_analyses_on_target_id_and_request_id'
+      begin
+        t.remove_index :name => 'index_analyses_on_target_id_and_request_id'
+      rescue
+      end
     end
     add_index :analyses, [:target_id, :request_id, :assay_id], :unique => false
   end
 
   def down
-    add_index :analyses, [:target_id, :request_id], :unique => true
     change_table :analyses do |t|
       t.remove_index :name => 'index_analyses_on_target_id_and_request_id_and_assay_id'
     end
